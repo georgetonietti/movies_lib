a4e89e8d389840e840073032ff00a23d218c8707
georgetonietti <georgetonietti@gmail.com>
2024-01-12 01:45:12 -0300

    First commit

---
+++ src/pages/Search.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect} from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+const searchUrl = import.meta.env.VITE_SEARCH;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+import './MoviesGrid.css';
+
+
+export default function Search() {
+
+    const [searchParams] = useSearchParams();
+
+    const [movies, setMovies] = useState([])
+    const query = searchParams.get("q")
+
+    const getSearchMovies = async (url) => {
+
+        const res = await fetch(url);
+        const data = await res.json();
+
+        setMovies(data.results);
+    }
+
+    useEffect(() => {
+        const searchQueryUrl = `${searchUrl}?${apiKey}&query=${query}`;
+        getSearchMovies(searchQueryUrl);
+    }, [query])
+
+    return (
+        <div className="container">
+            <h2 className="title">
+                Resultados para: <span className="query-text">{query}</span>
+            </h2>
+            <div className="movies-container">
+            {movies.length > 0 
+            ? movies.map((movie) => (<MovieCard key={movie.id} movie={movie}/>))
+            : <p>Carregando...</p>
+            }
+            </div>
+        </div>
+    )
+}
